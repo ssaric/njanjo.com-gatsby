d4409d4ec54011b5f77c0e06c4e20f5d15437b79
Sanjin Šarić <sanjin.saric1991@gmail.com>
2020-01-11 17:05:29 +0100

    add tracking

--- src/html.js
+++ src/html.js
@@ -11,6 +11,19 @@ export default function HTML(props) {
                     name="viewport"
                     content="width=device-width, initial-scale=1, shrink-to-fit=no"
                 />
+                <script>
+                    (function(f, a, t, h, o, m){
+                    a[h]=a[h]||function(){
+                        (a[h].q=a[h].q||[]).push(arguments)
+                    };
+                    o=f.createElement('script'),
+                    m=f.getElementsByTagName('script')[0];
+                    o.async=1; o.src=t; o.id='fathom-script';
+                    m.parentNode.insertBefore(o,m)
+                })(document, window, '//fathom.njanjo.com/tracker.js', 'fathom');
+                    fathom('set', 'siteId', 'HJMBL');
+                    fathom('trackPageview');
+                </script>
                 <script
                     type="text/javascript"
                     dangerouslySetInnerHTML={{
